c1f01b5a7116796ba2a6aab766ddc16d0ac5a625
ND-460 <22it460@bvmengineering.ac.in>
2025-07-10 12:01:12 +0530

    Adde mongodb connection and some routes

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
+//database connection
+const connectMoongoseDb = require('./config/db');
+connectMoongoseDb()
+const app = express();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+
+//routes
 app.get('/',(req,res) =>{
-    res.send('Hello World');
+    res.send('Hello World!');
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+
+//listening to the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on ${process.env.BASE_URL}:${port}`);
 }
 );
