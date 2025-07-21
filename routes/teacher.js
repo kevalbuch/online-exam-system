2c362e145b8c9ee586b4921d2934957a69095212
ND-460 <22it460@bvmengineering.ac.in>
2025-07-21 11:32:41 +0530

    completed testing of the teacher routes

--- routes/teacher.js
+++ routes/teacher.js
@@ -119,7 +119,7 @@ router.get("/profile/:profileID", auth, async (req, res) => {
   const profileID = req.params.profileID;
 
   try {
-    const obj = await Student.findOne({
+    const obj = await Teacher.findOne({
       _id: profileID,
     }).populate("profileInfo").exec();
 
@@ -200,29 +200,54 @@ router.post("/create-test", auth, async (req, res) => {
  * @description - Updating Test using testID
  */
 
+// router.put("/update-test/:testid", auth, async (req, res) => {
+//   const testID = req.params.testid;
+//   console.log(testID);
+//   const questionsData = req.body.questions;
+//   try {
+//     const testData = await Test.findOneAndUpdate(
+//       { _id: testID },
+//       { questions: questionsData },
+//       function (err, updatedData) {
+//         if (err) {
+//           return res.status(400).json({ message: "failed to update document" });
+//         } else {
+//           return res.status(200).json({
+//             message: "questions succesfully updated",
+//           });
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     console.log(err.message);
+//     res.status(500).send("Error in Updating");
+//   }
+// });
+
 router.put("/update-test/:testid", auth, async (req, res) => {
   const testID = req.params.testid;
   console.log(testID);
+
   const questionsData = req.body.questions;
+
   try {
-    const testData = await Test.findOneAndUpdate(
+    const updatedTest = await Test.findOneAndUpdate(
       { _id: testID },
       { questions: questionsData },
-      function (err, updatedData) {
-        if (err) {
-          return res.status(400).json({ message: "failed to update document" });
-        } else {
-          return res.status(200).json({
-            message: "questions succesfully updated",
-          });
-        }
-      }
+      { new: true }
     );
+
+    if (!updatedTest) {
+      return res.status(404).json({ message: "Test not found" });
+    }
+
+    res.status(200).json({ message: "Questions successfully updated" });
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Error in Updating");
   }
 });
+
 
 /**
  * @method - PUT
@@ -230,29 +255,55 @@ router.put("/update-test/:testid", auth, async (req, res) => {
  * @description - Updating Teacher profile using profileID
  */
 
+// router.put("/update-profile/:profileID", auth, async (req, res) => {
+//   const profileID = req.params.profileID;
+//   const { firstName, lastName, email, password, phone } = req.body;
+//   try {
+//     const testData = await Teacher.findOneAndUpdate(
+//       { _id: profileID },
+//       { ...req.body },
+//       function (err, updatedData) {
+//         if (err) {
+//           return res.status(400).json({ message: "failed to update profile" });
+//         } else {
+//           console.log(updatedData);
+//           return res.status(200).json({
+//             message: "profile succesfully updated",
+//           });
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     console.log(err.message);
+//     res.status(500).send("Error in Updating Profile");
+//   }
+// });
+
 router.put("/update-profile/:profileID", auth, async (req, res) => {
   const profileID = req.params.profileID;
   const { firstName, lastName, email, password, phone } = req.body;
+
   try {
-    const testData = await Teacher.findOneAndUpdate(
+    const updatedProfile = await Teacher.findOneAndUpdate(
       { _id: profileID },
       { ...req.body },
-      function (err, updatedData) {
-        if (err) {
-          return res.status(400).json({ message: "failed to update profile" });
-        } else {
-          console.log(updatedData);
-          return res.status(200).json({
-            message: "profile succesfully updated",
-          });
-        }
-      }
+      { new: true }
     );
+
+    if (!updatedProfile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    console.log(updatedProfile);
+    return res.status(200).json({
+      message: "Profile successfully updated",
+    });
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Error in Updating Profile");
   }
 });
+
 
 /**
  * @method - PUT
@@ -260,31 +311,53 @@ router.put("/update-profile/:profileID", auth, async (req, res) => {
  * @description - Fetching classes to which the test assigned using testID
  */
 
+// router.put("/assigend-to/:testID", auth, async (req, res) => {
+//   const testID = req.params.testID;
+//   const { className } = req.body;
+//   try {
+//     await Test.updateOne(
+//       { _id: testID },
+//       {
+//         $addToSet: { assignedTo: [...className] },
+//       },
+//       function (err, updatedData) {
+//         if (err) {
+//           return res
+//             .status(400)
+//             .json({ message: "failed to update assigendStudents" });
+//         } else {
+//           return res.status(200).json({
+//             updatedData,
+//           });
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     res.status(500).send("Error in Updating");
+//   }
+// });
+
 router.put("/assigend-to/:testID", auth, async (req, res) => {
   const testID = req.params.testID;
   const { className } = req.body;
+
   try {
-    await Test.updateOne(
+    const updatedData = await Test.updateOne(
       { _id: testID },
       {
-        $addToSet: { assignedTo: [...className] },
-      },
-      function (err, updatedData) {
-        if (err) {
-          return res
-            .status(400)
-            .json({ message: "failed to update assigendStudents" });
-        } else {
-          return res.status(200).json({
-            updatedData,
-          });
-        }
+        $addToSet: { assignedTo: { $each: className } }
       }
     );
+
+    return res.status(200).json({
+      updatedData
+    });
   } catch (err) {
+    console.log(err.message);
     res.status(500).send("Error in Updating");
   }
 });
+
 
 /**
  * @method - DELETE
@@ -292,19 +365,37 @@ router.put("/assigend-to/:testID", auth, async (req, res) => {
  * @description - Delete a particular test using testID
  */
 
+// router.delete("/delete-test/:testid", auth, async (req, res) => {
+//   const testID = req.params.testid;
+//   console.log(testID);
+//   try {
+//     const testData = await Test.findByIdAndDelete(testID, function (err) {
+//       if (err) {
+//         return res.status(400).json({ message: "failed to delete document" });
+//       } else {
+//         return res.status(200).json({
+//           message: "successfully deleted",
+//         });
+//       }
+//     });
+//   } catch (err) {
+//     console.log(err.message);
+//     res.status(500).send("Error in Deleting");
+//   }
+// });
+
 router.delete("/delete-test/:testid", auth, async (req, res) => {
   const testID = req.params.testid;
   console.log(testID);
+  
   try {
-    const testData = await Test.findByIdAndDelete(testID, function (err) {
-      if (err) {
-        return res.status(400).json({ message: "failed to delete document" });
-      } else {
-        return res.status(200).json({
-          message: "successfully deleted",
-        });
-      }
-    });
+    const deletedTest = await Test.findByIdAndDelete(testID);
+    
+    if (!deletedTest) {
+      return res.status(404).json({ message: "Test not found" });
+    }
+
+    res.status(200).json({ message: "Successfully deleted" });
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Error in Deleting");
